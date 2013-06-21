5dd0364c615914faabfa36b28e92dfa9cef21299
Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
2013-06-21 15:32:08 +0200

    adjust events

--- EventListener/UpdateArticleSubscriber.php
+++ EventListener/UpdateArticleSubscriber.php
@@ -18,38 +18,38 @@ class UpdateArticleSubscriber implements EventSubscriberInterface
 {
     public function clearCache(GenericEvent $event)
     {
-        if ($event->hasArgument('article')) {
+        if ($event->getName() == 'article.publish') {
+            $article = $article = new \Article($event->getArgument('number'), $event->getArgument('language'));
+        } else if ($event->getName() == 'article.update' || $event->getName() == 'Articles.update') {
+            $articleData = $event->getArgument('id');
+            $article = new \Article($articleData['Number'], $articleData['IdLanguage']);
+        } else if ($event->getName() == 'article.move') {
             $article = $event->getArgument('article');
-
-            if (!$article->isPublished()) {
-                return true;
-            }
-
-            $url = \ShortURL::GetURL(
-                $article->getPublicationId(),
-                $article->getLanguageId(),
-                $article->getIssueNumber(),
-                $article->getSectionNumber(),
-                $article->getArticleNumber()
-            );
-
-            $browser = new \Buzz\Browser(new \Buzz\Client\Curl());
-            $response = $browser->post('http://developers.facebook.com/tools/debug', array(
-                'user_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13'
-            ), http_build_query(array(
-                'q' => $url
-            )));
-            error_log(print_r($response, true));
         }
+
+        $url = \ShortURL::GetURL(
+            $article->getPublicationId(),
+            $article->getLanguageId(),
+            $article->getIssueNumber(),
+            $article->getSectionNumber(),
+            $article->getArticleNumber()
+        );
+
+        $browser = new \Buzz\Browser(new \Buzz\Client\Curl());
+        $response = $browser->post('http://developers.facebook.com/tools/debug', array(
+            'user_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13'
+        ), http_build_query(array(
+            'q' => $url
+        )));
     }
 
     public static function getSubscribedEvents()
     {
         return array(
+            'Articles.update' => array('clearCache', 1),
             'article.update' => array('clearCache', 1),
-            'articles.update' => array('clearCache', 1),
             'article.move' => array('clearCache', 1),
-            'article.publish' => array('clearCache', 1),
+            'article.publish' => array('clearCache', 1)
         );
     }
 }
